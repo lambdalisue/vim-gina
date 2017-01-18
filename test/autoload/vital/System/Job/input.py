03c26e1160e2d1144e7dbb2dca93174dba845b5f
lambdalisue <lambdalisue@hashnote.net>
2017-01-18 17:50:30 +0900

    Support python2 on test

--- test/autoload/vital/System/Job/input.py
+++ test/autoload/vital/System/Job/input.py
@@ -1,2 +1,7 @@
-name = input('Please input your name: ')
+import sys
+
+if sys.version_info >= (3, 0):
+    name = input('Please input your name: ')
+else:
+    name = raw_input('Please input your name: ')
 print('Hello %s' % name)
